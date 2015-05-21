781daf076a6e5bc699acdd18e3a10149af549aa4
David Cumps <david@cumps.be>
2015-05-21 15:29:08 +0200

    Add construct

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.0.3 - May 22 2015
+* Add construct
+
 #### 0.0.2 - May 21 2015
 * Remove errorState
 * Replace errorState by apply (<*>) and lift (<!>)
--- src/Exira.ErrorHandling/ErrorHandling.fs
+++ src/Exira.ErrorHandling/ErrorHandling.fs
@@ -5,39 +5,40 @@ module ErrorHandling =
         | Success of 'TSuccess
         | Failure of 'TFailure
 
-    // convert a single value into a two-track result
+    /// convert a single value into a two-track result
     let succeed x =
         Success x
 
-    // convert a single value into a two-track result
+    /// convert a single value into a two-track result
     let fail x =
         Failure x
 
+    /// convert a single value into a two-track async result
     let failAsync x =
         async { return Failure x }
 
-    // apply either a success function or failure function
+    /// apply either a success function or failure function
     let either successFunc failureFunc twoTrackInput =
         match twoTrackInput with
         | Success s -> successFunc s
         | Failure f -> failureFunc f
 
-    // convert a switch function into a two-track function
+    /// convert a switch function into a two-track function
     let bind f = either f fail
 
-    // convert a one-track function into a switch
+    /// convert a one-track function into a switch
     let switch f = f >> succeed
 
-    // convert a one-track function into a two-track function
+    /// convert a one-track function into a two-track function
     let map f = either (f >> succeed) fail
 
-    // convert a dead-end function into a one-track function
+    /// convert a dead-end function into a one-track function
     let tee f x = f x; x
 
-    // convert a switch function into a two-track function
+    /// convert a switch function into a two-track function
     let bindAsync f = either f failAsync
 
-    // map the messages to a different error type
+    /// map the messages to a different error type
     let mapMessages f result =
         match result with
         | Success x -> succeed x
@@ -66,3 +67,15 @@ module ErrorHandling =
 
     /// infix version of lift
     let (<!>) = lift
+
+    let private constructionSuccess value =
+        succeed value
+
+    let private constructionFailure value =
+        fail [value]
+
+    /// create with continuation
+    /// assumes ctor looks like: let createWithCont success failure value =
+    let construct ctor value =
+        value
+        |> ctor constructionSuccess constructionFailure
