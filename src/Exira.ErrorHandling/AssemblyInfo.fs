196c96c2c304b073795472e88fa0e521c8fb6650
David Cumps <david@cumps.be>
2015-05-21 20:28:29 +0200

    Bump version to 0.0.4

--- src/Exira.ErrorHandling/AssemblyInfo.fs
+++ src/Exira.ErrorHandling/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.ErrorHandling")>]
 [<assembly: AssemblyProductAttribute("Exira.ErrorHandling")>]
 [<assembly: AssemblyDescriptionAttribute("F# Railway Oriented Programming Helpers")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.3"
+    let [<Literal>] Version = "0.0.4"
