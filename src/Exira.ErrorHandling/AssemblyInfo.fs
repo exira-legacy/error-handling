a79cf4339291d35b50a8059d8b9734e254734082
Team City <info@exira.com>
2015-09-30 21:31:54 +0000

    Bump version to 0.1.25

--- src/Exira.ErrorHandling/AssemblyInfo.fs
+++ src/Exira.ErrorHandling/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.ErrorHandling")>]
 [<assembly: AssemblyProductAttribute("Exira.ErrorHandling")>]
 [<assembly: AssemblyDescriptionAttribute("F# Railway Oriented Programming Helpers")>]
-[<assembly: AssemblyVersionAttribute("0.1.23")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.23")>]
-[<assembly: AssemblyMetadataAttribute("githash","23d74c8bea64ffa7e6f7f40161af119bff0d4aa9")>]
+[<assembly: AssemblyVersionAttribute("0.1.25")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.25")>]
+[<assembly: AssemblyMetadataAttribute("githash","5b31a1c9f45d38c31212ecf82529f79183ee9adc")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.23"
+    let [<Literal>] Version = "0.1.25"
