1db1c77f2fb64f1a08e6e49815cc4ccce794c018
David Cumps <david@cumps.be>
2015-09-30 21:31:45 +0200

    Move to TC

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.1 - September 30 2015
+* Switch everything over to our TeamCity/Octopus Deploy infrastructure
+
 #### 0.0.5 - September 26 2015
 * Upgrade dependencies
 * Use F# 4.0
--- src/Exira.ErrorHandling/AssemblyInfo.fs
+++ src/Exira.ErrorHandling/AssemblyInfo.fs
@@ -4,9 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.ErrorHandling")>]
 [<assembly: AssemblyProductAttribute("Exira.ErrorHandling")>]
 [<assembly: AssemblyDescriptionAttribute("F# Railway Oriented Programming Helpers")>]
-[<assembly: AssemblyVersionAttribute("0.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","49ccc0c05ed6e75cc545e368ff1c64b04ca7ea4b")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.5"
+    let [<Literal>] Version = "0.1.0"
