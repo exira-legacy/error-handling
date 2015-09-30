c2f976a1bbd005dab1d0c8f8c1b88977950ad947
David Cumps <david@cumps.be>
2015-10-01 00:04:06 +0200

    Remove Travis badge

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -4,7 +4,7 @@
 #I "../../bin"
 
 (**
-error-handling [![Travis build status](https://travis-ci.org/exira/error-handling.png)](https://travis-ci.org/exira/error-handling) [![NuGet Status](http://img.shields.io/nuget/v/Exira.ErrorHandling.svg?style=flat)](https://www.nuget.org/packages/Exira.ErrorHandling/)
+error-handling [![NuGet Status](http://img.shields.io/nuget/v/Exira.ErrorHandling.svg?style=flat)](https://www.nuget.org/packages/Exira.ErrorHandling/)
 ======================
 
 <div class="row">
